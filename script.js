bc366a3d2117b4a8cb4f89caf2539c088f983e04
AkkashSR <akkashsr@gmail.com>
2020-10-03 21:54:33 +0530

    Final changes to the game

--- script.js
+++ script.js
@@ -1,126 +1,129 @@
 let noOfRounds = 0;
-
+let playerScore = 0;
+let computerScore = 0;
 let playerSelection;
 let computerSelection;
-let roundWinner;
-let gameWinner;
-let computerWin = 0;
-let playerWin = 0;
+let gameOver = false;
 
-const gameSelectButtonGrid = document.getElementById('game-select-buttons-grid');
-const gameGrid = document.getElementById('game-grid');
-const roundWinnerDiv = document.getElementById('round-winner');
-const playerAndComputerScoresDiv = document.getElementById('player-and-computer-scores');
-const playerScore = document.getElementById('player-score');
-const computerScore = document.getElementById('computer-score');
-const computerSelectionText = document.getElementById('computer-selection-text');
-const playerSelectionText = document.getElementById('player-selection-text');
-const roundWinnerText = document.getElementById('round-winner-text');
-const gameWinnerText = document.getElementById('game-winner-text');
-const continueGameBtn = document.getElementById('game-continue-button');
-const newGameBtn = document.getElementById('new-game-button');
-const rockBtn = document.getElementById('rock');
-const paperBtn = document.getElementById('paper');
-const scissorsBtn = document.getElementById('scissors');
-console.log(gameSelectButtonGrid);
-console.log(gameGrid);
+const computerChoices = ["Rock", "Paper", "Scissors"];
 
-const computerPlay = () => {
-	const choices = ['rock', 'paper', 'scissors'];
-	return choices[Math.floor(Math.random() * 3)];
-};
+const bo3 = document.getElementById("bo3");
+const bo5 = document.getElementById("bo5");
+const bo7 = document.getElementById("bo7");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerSelectionText = document.getElementById("player-selection");
+const computerSelectionText = document.getElementById("computer-selection");
+const roundWinnerText = document.getElementById("round-winner");
+const gameWinnerText = document.getElementById("game-winner");
+const playerScoreText = document.getElementById("player-score");
+const computerScoreText = document.getElementById("computer-score");
 
-function game(rounds){
-  while(rounds !== 0){
-    rockBtn.addEventListener('click', (event) => {
-    console.log(event);
-    playerSelection = 'rock';
-    console.log(playerSelection);
-    computerSelection = computerPlay();
-    console.log(computerSelection);
-    gameGrid.classList.toggle('visible');
-    gameGrid.classList.toggle('invisible');
-    playerAndComputerScoresDiv.classList.toggle('visible');
-    playerAndComputerScoresDiv.classList.toggle('invisble');
-    });
-    
-    paperBtn.addEventListener('click', () => {
-    playerSelection = 'paper';
-    console.log(playerSelection);
-    computerSelection = computerPlay();
-    console.log(computerSelection);
-    gameGrid.classList.toggle('visible');
-    gameGrid.classList.toggle('invisible');
-    playerAndComputerScoresDiv.classList.toggle('visible');
-    playerAndComputerScoresDiv.classList.toggle('invisble');
-    });
-    
-    scissorsBtn.addEventListener('click', () => {
-    playerSelection = 'scissors';
-    console.log(playerSelection);
-    computerSelection = computerPlay();
-    console.log(computerSelection);
-    gameGrid.classList.toggle('visible');
-    gameGrid.classList.toggle('invisible');
-    playerAndComputerScoresDiv.classList.toggle('visible');
-    playerAndComputerScoresDiv.classList.toggle('invisble');
-    });
-
-    playerSelectionText.innerText = `Player selected ${playerSelection}`;
-    computerSelectionText.innerText = `Computer selected ${computerSelection}`;
-
-
-    if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock'){
-      roundWinner = 'Player';
-      roundWinnerText.textContent = `The winner is ${roundWinner}`;
-      playerWin += 1;
-      playerScore.textContent = `Player Score: ${playerWin}`;
+function checkGameWin(playerScore, computerScore, roundsRemaining){
+  if((playerScore - computerScore) > noOfRounds / 2 || roundsRemaining === 0){
+    if(playerScore > computerScore){
+      gameWinnerText.textContent = "Game Winner : Player";
     }
     else{
-      roundWinner = 'Computer';
-      roundWinnerText.textContent = `The winner is ${roundWinner}`;
-      playerWin += 1;
-      playerScore.textContent = `Player Score: ${playerWin}`;
+      gameWinnerText.textContent = "Game Winner: Computer";
     }
+  }
+  gameWin = true;
+}
 
-    rounds -= 1;
+function checkRoundWin(playerSelection, computerSelection, roundsRemaining) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    roundsRemaining -= 1;
+    console.log(roundsRemaining);
+    playerScore += 1;
+    roundWinnerText.textContent = "Round Winner : Player";
+    playerScoreText.textContent = `Player Score : ${playerScore}`;
+    computerScoreText.textContent = `Computer Score : ${computerScore}`;
+  } else if (
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper") ||
+    (computerSelection === "Paper" && playerSelection === "Rock")
+  ) {
+    roundsRemaining -= 1;
+    console.log(roundsRemaining);
+    computerScore += 1;
+    roundWinnerText.textContent = "Round Winner : Computer";
+    playerScoreText.textContent = `Player Score : ${playerScore}`;
+    computerScoreText.textContent = `Computer Score : ${computerScore}`;
+  } else {
+    console.log(roundsRemaining);
+    roundWinnerText.textContent = "Round Winner : None";
+    playerScoreText.textContent = `Player Score : ${playerScore}`;
+    computerScoreText.textContent = `Computer Score : ${computerScore}`;
+  }
+  checkGameWin(playerScore, computerScore, roundsRemaining);
+  return;
+}
 
-    console.log(rounds);
+function playRound(roundsRemaining) {
+  rock.onclick = () => {
+    playerSelection = "Rock";
+    computerSelection = computerChoices[Math.floor(Math.random() * 3)];
+    playerSelectionText.textContent = `Player Selected : ${playerSelection} `;
+    computerSelectionText.textContent = `Computer Selected : ${computerSelection}`;
+    console.log(playerSelection, computerSelection);
+    checkRoundWin(playerSelection, computerSelection, roundsRemaining);
+  };
+  paper.onclick = () => {
+    playerSelection = "Paper";
+    computerSelection = computerChoices[Math.floor(Math.random() * 3)];
+    playerSelectionText.textContent = `Player Selected : ${playerSelection} `;
+    computerSelectionText.textContent = `Computer Selected : ${computerSelection}`;
+    console.log(playerSelection, computerSelection);
+    checkRoundWin(playerSelection, computerSelection, roundsRemaining);
+  };
+  scissors.onclick = () => {
+    playerSelection = "Scissors";
+    computerSelection = computerChoices[Math.floor(Math.random() * 3)];
+    playerSelectionText.textContent = `Player Selected : ${playerSelection} `;
+    computerSelectionText.textContent = `Computer Selected : ${computerSelection}`;
+    console.log(playerSelection, computerSelection);
+    checkRoundWin(playerSelection, computerSelection, roundsRemaining);
+  };
+  if(gameOver === true){
+    return;
   }
 }
 
-document.querySelector('#bo3').addEventListener('click', () => {
+bo3.onclick = () => {
   noOfRounds = 3;
-  console.log(noOfRounds);
-  gameSelectButtonGrid.classList.toggle('visible');
-  gameSelectButtonGrid.classList.toggle('invisible');
-  gameGrid.classList.toggle('visible');
-  gameGrid.classList.toggle('invisible');
-  game(noOfRounds);
-});
+  bo3.disabled = true;
+  bo5.disabled = true;
+  bo7.disabled = true;
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+  playRound(noOfRounds);
+};
 
-document.querySelector('#bo5').addEventListener('click', () => {
+bo5.onclick = () => {
   noOfRounds = 5;
-  console.log(noOfRounds);
-  gameSelectButtonGrid.classList.toggle('visible');
-  gameSelectButtonGrid.classList.toggle('invisible');
-  gameGrid.classList.toggle('visible');
-  gameGrid.classList.toggle('invisible');
-  game(noOfRounds);
-});
+  bo3.disabled = true;
+  bo5.disabled = true;
+  bo7.disabled = true;
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+  playRound(noOfRounds);
+};
 
-document.querySelector('#bo7').addEventListener('click', () => {
+bo7.onclick = () => {
   noOfRounds = 7;
-  console.log(noOfRounds);
-  gameSelectButtonGrid.classList.toggle('visible');
-  gameSelectButtonGrid.classList.toggle('invisible');
-  gameGrid.classList.toggle('visible');
-  gameGrid.classList.toggle('invisible');
-  game(noOfRounds);
-});
-
-
-
-
-
-
+  bo3.disabled = true;
+  bo5.disabled = true;
+  bo7.disabled = true;
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+  playRound(noOfRounds);
+};
